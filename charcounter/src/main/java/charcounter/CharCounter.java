370c174958249419d6821650c87fe385f2666578
Evgeny Sabarov <e.sabarov@gmail.com>
2021-10-27 18:34:53 +0300

    changed return to Cache

--- charcounter/src/main/java/charcounter/CharCounter.java
+++ charcounter/src/main/java/charcounter/CharCounter.java
@@ -8,7 +8,7 @@ public class CharCounter {
     
     private static final String EMPTY = "";
     
-    public Map<Character, Integer> charCounting(String line) {
+    protected Map<Character, Integer> charCounting(String line) {
         if (line == null) { 
             throw new IllegalArgumentException();
         }
@@ -16,11 +16,11 @@ public class CharCounter {
             return Cache.cache.get(line);
         }
         char[] symbols = line.toCharArray();
-        Map<Character, Integer> resultMap = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         for(char symbol : symbols) {
-            resultMap.put(symbol, (int) Arrays.stream(line.split(EMPTY)).filter(s -> s.equals(symbol)).count());
+            map.put(symbol, (int) Arrays.stream(line.split(EMPTY)).filter(s -> s.equals("" + symbol)).count());
         }
-        Cache.cache.put(line, resultMap);
-        return resultMap;   
+        Cache.cache.put(line, map);
+        return Cache.cache.get(line);   
     }
 }
