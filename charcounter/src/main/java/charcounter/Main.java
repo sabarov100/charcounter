f1eb5ebd6f11a26457d1f1057bc1eee8ae968757
Evgeny Sabarov <e.sabarov@gmail.com>
2021-10-27 18:41:23 +0300

    changed to Facade object

--- charcounter/src/main/java/charcounter/Main.java
+++ charcounter/src/main/java/charcounter/Main.java
@@ -3,9 +3,8 @@ package charcounter;
 public class Main {
 
     public static void main(String[] args) {
-        CharCounter charCounter = new CharCounter();
-        System.out.println(charCounter.charCounting("Hello world!"));
-        
+        Facade facade = new Facade();
+        System.out.println(facade.charCounterUserInterface("Hello world!"));
     }
-
+    
 }
