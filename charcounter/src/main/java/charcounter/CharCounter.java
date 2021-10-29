81fa3b963b3a96d87d8443fcf18f8a7bf2bf6dc4
Evgeny Sabarov <e.sabarov@gmail.com>
2021-10-29 14:52:24 +0300

    deleted checking of cache

--- charcounter/src/main/java/charcounter/CharCounter.java
+++ charcounter/src/main/java/charcounter/CharCounter.java
@@ -12,15 +12,12 @@ public class CharCounter {
         if (line == null) { 
             throw new IllegalArgumentException();
         }
-        if(Cache.cache.containsKey(line)) {
-            return Cache.cache.get(line);
-        }
         char[] symbols = line.toCharArray();
         Map<Character, Integer> map = new HashMap<>();
         for(char symbol : symbols) {
             map.put(symbol, (int) Arrays.stream(line.split(EMPTY)).filter(s -> s.equals("" + symbol)).count());
         }
         Cache.cache.put(line, map);
-        return Cache.cache.get(line);   
+        return map;   
     }
 }
