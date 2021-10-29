96f193610bd13c1ea9f7f90ee9b9e774880d847a
Evgeny Sabarov <e.sabarov@gmail.com>
2021-10-29 14:54:39 +0300

    added checking of cache

--- charcounter/src/main/java/charcounter/Formatter.java
+++ charcounter/src/main/java/charcounter/Formatter.java
@@ -1,8 +1,6 @@
 package charcounter;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,7 +8,7 @@ import java.util.stream.Stream;
 
 public class Formatter {
     
-    
+    private CharCounter charCounter;
     private static final String EMPTY = "";
     private static final String QUOTATION_MARK = "\"";
     private static final String EQUAL_SING = " = ";
@@ -19,8 +17,12 @@ public class Formatter {
     public String formatCharCounting(String lineInput, Map<Character, Integer> mapInput) {
         if (lineInput == null) { 
             throw new IllegalArgumentException();
-        }         
-        mapInput = Cache.cache.get(lineInput);
+        } 
+        if(Cache.cache.containsKey(lineInput)) {
+            mapInput = Cache.cache.get(lineInput);
+        } else {
+            mapInput = charCounter.charCounting(lineInput);
+        }
         StringBuilder sb = new StringBuilder();
         Stream<String> streamOfArray = Arrays.stream(lineInput.split(EMPTY));
         List<String> list = streamOfArray.map(s->s.split(EMPTY)).flatMap(Arrays::stream)
