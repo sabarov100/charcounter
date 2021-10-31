d59f50396447ba0da75534a41161605748e3cd49
Evgeny Sabarov <e.sabarov@gmail.com>
2021-10-31 10:10:27 +0300

    changed fields location

--- charcounter/src/main/java/charcounter/Facade.java
+++ charcounter/src/main/java/charcounter/Facade.java
@@ -1,15 +1,15 @@
 package charcounter;
 
 public class Facade {
-    
+
+    private CharCounter charCounter;
+    private Formatter formatter;
+
     public Facade(CharCounter charCounter, Formatter formatter) {
         super();
         this.charCounter = charCounter;
         this.formatter = formatter;
     }
-
-    private CharCounter charCounter;
-    private Formatter formatter;
     
     public String charCounterUserInterface(String lineInput) {
         return formatter.formatCharCounting(lineInput, charCounter.charCounting(lineInput));
